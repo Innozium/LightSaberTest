a530accf617b41347fd4b109fd7a2936c4bbb771
innozium2 <zium349@daum.net>
2017-04-06 16:24:43 +0900

    170406 pm 0423 Update
    170406 pm 0423 최재원
    광선검테스트.
    클릭을 하고 있으면 잔상이 남는 초기 0.1초에서
    최대 0.4 시간이 늘어납니다.

---
+++ Assets/02.Scripts/TrailCtrl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailCtrl : MonoBehaviour {
+
+    private Transform tr;
+    private TrailRenderer trailRenderer;
+
+    private bool is_DownCheck;
+    private float firstTime = 0.0f;
+
+    private void Awake()
+    {
+        tr = GetComponent<Transform>();
+        trailRenderer = GetComponent<TrailRenderer>();
+    }
+    
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetMouseButton(0))
+        {
+            if (!is_DownCheck) is_DownCheck = true;
+            //print("dd");
+            Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            clickPosition.z = 0;
+
+            //print(Input.mousePosition);
+            //print(clickPosition);
+
+            tr.position = clickPosition;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            is_DownCheck = false;
+            firstTime = 0.0f;
+            trailRenderer.time = 0.1f;
+        }
+
+        if(is_DownCheck)
+        {
+            firstTime += Time.deltaTime;
+        }
+
+        if(firstTime > 0.5f)
+        {
+            if (trailRenderer.time < 0.5f) trailRenderer.time += 0.1f;
+            else if (trailRenderer.time >= 0.5f) trailRenderer.time = 0.4f;
+        }
+
+	}
+}
